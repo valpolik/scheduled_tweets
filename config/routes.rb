76415379c46a152ffe402a22de439217ac866bf7
Valentin Polikarpov <>
2022-01-19 06:09:40 +0300

    Created a Sign Up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
   # GET /about
   # get "/about", to: "about#index"
   # get "about-us", to: "about#index", as: :about
   # get "about", to: "about#index", as: :about
   get "about", to: "about#index"
 
+  # GET /sign_up
+  get "sign_up", to: "registrations#new"
+
+  post "sign_up", to: "registrations#create"
+
   # GET /
   # get "/",     to: "main#index"
   # get "",     to: "main#index"
   root to: "main#index"
-
 end
